f07680c249078772cc68253c38e7616f5bc34515
Maksym Chaika <maksym.chaika@howly.com>
2023-08-18 14:27:19 +0300

    feat: updated workflow

--- mac/nvim/lua/fable/mycolors.lua
+++
@@ -1,7 +0,0 @@
-vim.o.background = "dark" -- or "light" for light mode
-
--- vim.cmd([[colorscheme gruvbox]])
--- vim.cmd([[colorscheme oxocarbon]])
-vim.cmd [[colorscheme moonfly]]
--- vim.cmd [[colorscheme catppuccin]]
--- vim.cmd [[colorscheme rose-pine]]
---
+++ nvim/lua/fable/packer.lua
@@ -0,0 +1,82 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+
+
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+-- Appearance
+	use 'ellisonleao/gruvbox.nvim'
+    use 'nyoom-engineering/oxocarbon.nvim'
+    use { "bluz71/vim-moonfly-colors", as = "moonfly" }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use { "xiyaowong/transparent.nvim", as = "transparent" }
+
+-- Navigation
+	use 'theprimeagen/harpoon'
+	use 'mbbill/undotree'
+
+-- Debugging
+    use 'mfussenegger/nvim-dap'
+    use 'leoluz/nvim-dap-go'
+    use 'rcarriga/nvim-dap-ui'
+    use 'theHamsta/nvim-dap-virtual-text'
+
+-- Git related plugins
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb'
+    use 'lewis6991/gitsigns.nvim'
+
+    use 'nvim-telescope/telescope-dap.nvim'
+
+-- Languages support
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+
+    -- Golang
+    use 'ray-x/go.nvim'
+    use 'ray-x/guihua.lua'
+
+    -- Database
+    use 'tpope/vim-dadbod'
+    use 'kristijanhusak/vim-dadbod-ui'
+    use 'kristijanhusak/vim-dadbod-completion'
+    use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
+
+    -- Helm
+    use 'towolf/vim-helm'
+
+end)
